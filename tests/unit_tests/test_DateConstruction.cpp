c37a800075881cd52d13fabd0eec86ab614ac618
Darius Arnold <mail@dariusarnold.de>
2018-08-20 16:16:42 +0200

    [add] comments describing tests for Date construction

--- tests/unit_tests/test_DateConstruction.cpp
+++ tests/unit_tests/test_DateConstruction.cpp
@@ -1,6 +1,10 @@
 #include "gtest/gtest.h"
 #include "Date.h"
 
+/**
+ * Test if the default constructor initializes the fields
+ * TODO: This is implementation dependent, is there another way to test for initialized values?
+ */
 TEST(TestDateConstruction, TestDefaultConstructor){
     Date da;
     EXPECT_EQ(da.getDoy(), 1);
@@ -9,6 +13,9 @@ TEST(TestDateConstruction, TestDefaultConstructor){
     EXPECT_EQ(da.getDay(), 1);
 }
 
+/**
+ * Test if setting the members given in the Year/Month/Day constructor works correctly
+ */
 TEST(TestDateConstruction, TestParameterDayConstructorSettingMembers){
     int year = 2008;
     int month = 9;
@@ -19,6 +26,10 @@ TEST(TestDateConstruction, TestParameterDayConstructorSettingMembers){
     EXPECT_EQ(d.getDay(), day);
 }
 
+/**
+ * Test if setting the members not given in the Year/Month/Day constructor by calculating them
+ * works correctly and takes the leap year into account
+ */
 TEST(TestDateConstruction, TestParameterDayConstructorDoyCalculationLeapYear){
     int year = 2008;
     int month = 9;
@@ -28,6 +39,10 @@ TEST(TestDateConstruction, TestParameterDayConstructorDoyCalculationLeapYear){
     EXPECT_EQ(d.getDoy(), doy);
 }
 
+/**
+ * Test if setting the members not given in the Year/Month/Day constructor by calculating them
+ * works correctly and takes the leap year into account
+ */
 TEST(TestDateConstruction, TestParameterDayConstructorDoyCalculationNoLeapYear){
     int year = 1999;
     int month = 12;
@@ -37,6 +52,9 @@ TEST(TestDateConstruction, TestParameterDayConstructorDoyCalculationNoLeapYear){
     EXPECT_EQ(d.getDoy(), doy);
 }
 
+/**
+ * Test if the Year/Doy constructor sets the given parameters correctly
+ */
 TEST(TestDateConstruction, TestParameterDoyConstructorSettingMembers){
     int year = 2000;
     int doy = 1;
@@ -45,10 +63,25 @@ TEST(TestDateConstruction, TestParameterDoyConstructorSettingMembers){
     EXPECT_EQ(d.getDoy(), doy);
 }
 
-TEST(TestDateConstruction, TestParameterDayConstructorDayCalculationLeapYear){
+/**
+ * Test if the Year/Doy constructor calculates the missing member values Month and Day of Month correctly
+ */
+TEST(TestDateConstruction, TestParameterDayConstructorDayCalculation){
     int year = 2000;
     int doy = 1;
     Date d(year, doy);
     EXPECT_EQ(d.getMonth(), 1);
     EXPECT_EQ(d.getDay(), 1);
+}
+
+/**
+ * Test if the Year/Doy constructor calculates the missing member values Month and Day of Month correctly,
+ * taking the additional leap day into account
+ */
+TEST(TestDateConstruction, TestParameterDayConstructorDayCalculationLeapYear){
+    int year = 2000;
+    int doy = 366;
+    Date d(year, doy);
+    EXPECT_EQ(d.getMonth(), 12);
+    EXPECT_EQ(d.getDay(), 31);
 }
